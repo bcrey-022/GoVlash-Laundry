72ec135f8de30f98d8a05a65eb73d616f5c84154
Brian C <briancaseyreynard22@gmail.com>
2025-10-25 11:25:16 +0700

    Initialize file

---
+++ src/main/Main.java
@@ -0,0 +1,24 @@
+package main;
+
+public class Main {
+	public void validUser() {
+		
+	}
+	public void register() {
+		String name;
+		String email;
+		String password;
+		String confirm;
+		String gender;
+		String role;
+		
+	}
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		new Main();
+	}
+
+}
---
+++ src/model/User.java
@@ -0,0 +1,66 @@
+package model;
+
+import java.sql.Date;
+
+public class User {
+	int id;
+	String name;
+	String email;
+	String password;
+	String gender;
+	Date dob;
+	String role;
+	public User(int id, String name, String email, String password, String gender, Date dob, String role) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.gender = gender;
+		this.dob = dob;
+		this.role = role;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+}
